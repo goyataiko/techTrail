f25fc9982be9c2fe2cdd2c1396bd17b665417ab6
Takano Computer <goyataiko@gmail.com>
2023-09-09 20:51:40 +0900

    profile edit ページ

--- resources/views/admin/profile/profile.blade.php
+++ resources/views/admin/profile/profile.blade.php
@@ -10,7 +10,7 @@
         </div>
     </div>
     <div class="section-body">
-        <h2 class="section-title">Hi, Ujang!</h2>
+        <h2 class="section-title">Hi, {{$user->name}}!</h2>
         <p class="section-lead">
             Change information about yourself on this page.
         </p>
@@ -18,25 +18,78 @@
         <div class="row mt-sm-4">
             <div class="col-12 col-md-12 col-lg-12">
                 <div class="card">
-                    <form method="post" class="needs-validation" novalidate="">@csrf
-                        <div class="card-header">
-                            <h4>Edit Profile</h4>
-                        </div>
-                        <div class="card-body">
+                    <div class="card-header">
+                        <h4>Profile Information</h4>
+                    </div>
+                    <div class="card-body">
+                        <form method="post" action="{{ route('profile.update') }}">
+                            @csrf
+                            @method('patch')
+
                             <div class="row">
                                 <div class="form-group col-md-6 col-12">
-                                    <label>First Name</label>
-                                    <input type="text" class="form-control" value="Ujang" required="">
-                                    <div class="invalid-feedback">
-                                        Please fill in the first name
-                                    </div>
+                                    <label>Name</label>
+                                    <input type="text" name="name" class="form-control" value="{{old('name', $user->name)}}" autofocus required="">
+                                    @if($errors->has('name'))
+                                    <code>{{$errors->first('name')}}</code>
+                                    @endif
+                                </div>
+
+                                <div class="form-group col-md-6 col-12">
+                                    <label>Email</label>
+                                    <input type="email" name="email" class="form-control" value="{{old('email', $user->email)}}" required="">
+                                    @if($errors->has('email'))
+                                    <code>{{$errors->first('email')}}</code>
+                                    @endif
+                                </div>
+
+                            </div>
+                            <div class="card-footer text-right">
+                                <button class="btn btn-primary">Save Changes</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="card">
+
+                    <div class="card-header">
+                        <h4>Update Password</h4>
+                    </div>
+                    <div class="card-body">
+                        <form method="post" action="{{ route('password.update') }}">
+                            @csrf
+                            @method('put')
+                            <div class="row">
+                                <div class="form-group col-12">
+                                    <label>Current Password</label>
+                                    <input type="password" name="current_password" class="form-control" required="" autocomplete="current-password">
+                                    @if($errors->updatePassword->has('current_password'))
+                                    <code>{{$errors->updatePassword->first('current_password')}}</code>
+                                    @endif
+                                </div>
+
+                                <div class="form-group col-12">
+                                    <label>New Password</label>
+                                    <input type="password" name="password" class="form-control" required="" autocomplete="new-password">
+                                    @if($errors->updatePassword->has('password'))
+                                    <code>{{$errors->updatePassword->first('password')}}</code>
+                                    @endif
+                                </div>
+                                <div class="form-group col-12">
+                                    <label>Confirm Password</label>
+                                    <input type="password" name="password_confirmation" class="form-control" required="" autocomplete="new-password">
+                                    @if($errors->updatePassword->has('password_confirmation'))
+                                    <code>{{$errors->updatePassword->first('password_confirmation')}}</code>
+                                    @endif
                                 </div>
                             </div>
-                        </div>
-                        <div class="card-footer text-right">
-                            <button class="btn btn-primary">Save Changes</button>
-                        </div>
-                    </form>
+                            <div class="card-footer text-right">
+                                <button class="btn btn-primary">Save Changes</button>
+                            </div>
+                        </form>
+
+                    </div>
                 </div>
             </div>
         </div>
