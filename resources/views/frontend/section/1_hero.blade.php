afe85682d18012a3fa210c9a9973423cdb1f5726
Takano Computer <goyataiko@gmail.com>
2023-09-15 19:26:26 +0900

    デザイン関連

--- resources/views/frontend/section/1_hero.blade.php
+++ resources/views/frontend/section/1_hero.blade.php
@@ -6,7 +6,7 @@
             <div class="col-lg-8">
                 <div class="header-text">
                     <h3 class="typer-title wow fadeInUp" data-wow-delay="0.2s">I'm ui/ux designer</h3>
-                    <h1 class="title wow fadeInUp" data-wow-delay="0.3s">{{ $hero->title }}</h1>
+                    <h1 class="title wow fadeInUp" data-wow-delay="0.3s">{!! $hero->title !!}</h1>
                     <div class="desc wow fadeInUp" data-wow-delay="0.4s">
                         <p>{{ $hero->sub_title }}</p>
                     </div>
